2d025f4282c7145b61c1198a151f209bfcb38080
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-12-02 20:46:33 -0700

    Changed navbar to include appropriate login/logout links depending on whether or not a user is logged in.

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,38 +1,50 @@
 import React from "react";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../../actions/userActions";
 
 const NavBar = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <Navbar bg="primary" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Budget Pal</Navbar.Brand>
+      <LinkContainer to="/">
+        <Navbar.Brand>Budget Pal</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+        <Nav className="ml-auto">
+          <LinkContainer to="/budget">
+            <Nav.Link>Budget</Nav.Link>
+          </LinkContainer>
+          <NavDropdown title="Transactions" id="basic-nav-dropdown">
+            <LinkContainer to="/transactions/new">
+              <NavDropdown.Item>New Transaction</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/transactions/all">
+              <NavDropdown.Item>View Transactions</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
+          {userInfo ? (
+            <LinkContainer to="/">
+              <Nav.Link className="ml-auto" onClick={logoutHandler}>
+                Logout
+              </Nav.Link>
+            </LinkContainer>
+          ) : (
+            <LinkContainer to="/login">
+              <Nav.Link className="ml-auto">Login</Nav.Link>
+            </LinkContainer>
+          )}
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
